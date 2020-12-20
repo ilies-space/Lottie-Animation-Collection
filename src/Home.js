29bb1f82d39ff8e63429fdec9e2a011f5f176d88
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-12-20 16:04:59 +0100

    emptybox exmple

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import LottieView from 'lottie-react-native';
 
-const App = () => {
+const Home = () => {
   return (
     <View style={{felx: 1}}>
-      <LottieView source={require('../assets/emptybox.json')} autoPlay loop />
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <LottieView
+          style={{height: '100%', width: '100%'}}
+          source={require('./assets/emptybox.json')}
+          autoPlay
+          loop
+        />
+      </View>
     </View>
   );
 };
 
-export default App;
+export default Home;
